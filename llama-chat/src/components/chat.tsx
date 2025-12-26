3b0e04951de7901adae445344795b8a2206e6928
Valentin Manes <valentinmanes@outlook.fr>
2025-12-26 15:05:03 +0100

    develop the chat

---
+++ llama-chat/src/components/chat.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { ChatSection as ChatSectionUI } from '@llamaindex/chat-ui'
+import '@llamaindex/chat-ui/styles/markdown.css'
+import '@llamaindex/chat-ui/styles/editor.css'
+import { useChat } from '@ai-sdk/react'
+
+export function ChatSection() {
+  const handler = useChat()
+
+  return (
+    <div className="flex h-[80vh] flex-col">
+      <ChatSectionUI handler={handler} />
+    </div>
+  )
+}
